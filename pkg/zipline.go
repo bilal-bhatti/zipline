5a826f9de0407e46c2ae2cfd4f1efcbe219e8867
Bilal Bhatti <bbhatti@gmail.com>
2020-01-27 21:22:34 -0600

    minor

--- pkg/zipline.go
+++ pkg/zipline.go
@@ -2,10 +2,12 @@ package zipline
 
 import "net/http"
 
+// Post marker for code gen
 func Post(handler interface{}) func(w http.ResponseWriter, req *http.Request) {
-	panic("Implementation not generated, run restify")
+	panic("Implementation not generated, run zipline")
 }
 
+// Get marker for code gen
 func Get(handler interface{}) func(w http.ResponseWriter, req *http.Request) {
-	panic("Implementation not generated, run restify")
+	panic("Implementation not generated, run zipline")
 }
