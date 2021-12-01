aa8f98f0815f2d40d370afc514f3eab02585d1b2
Bilal Bhatti <bbhatti@gmail.com>
2021-11-30 21:02:45 -0600

    minor refactoring

---
+++ cmd/zipline/gen_cmd.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/bilal-bhatti/zipline/internal/debug"
+
+	"github.com/bilal-bhatti/zipline/internal"
+	"github.com/google/subcommands"
+)
+
+type genCmd struct {
+	debug bool
+}
+
+func (*genCmd) Name() string { return "gen" }
+
+func (*genCmd) Synopsis() string {
+	return "generate handler functions and Open API specs from Go code"
+}
+
+func (*genCmd) Usage() string {
+	return `
+
+gen [packages]:
+
+generate documentation and bindings for template in specified package, defaults to "."
+	example: "zipline ./..."
+	example: "zipline gen ./..."
+	example: "zipline gen -d ./..."
+
+`
+}
+
+func (p *genCmd) SetFlags(f *flag.FlagSet) {
+	f.BoolVar(&p.debug, "d", false, "run with trace logging enabled")
+}
+
+func (p *genCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	zipline := internal.NewZipline()
+
+	debug.Debug = p.debug
+
+	err := zipline.Start(packageParam(f))
+
+	if err != nil {
+		log.Println(fmt.Errorf("%s", err.Error()))
+	}
+
+	log.Println("-----")
+	return subcommands.ExitSuccess
+}
--- cmd/zipline/main.go
+++ cmd/zipline/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/bilal-bhatti/zipline/internal/debug"
 
-	"github.com/bilal-bhatti/zipline/internal"
 	"github.com/google/subcommands"
 )
 
@@ -17,7 +14,7 @@ var Version = "DEV"
 
 func main() {
 	subcommands.Register(subcommands.HelpCommand(), "")
-	subcommands.Register(subcommands.FlagsCommand(), "")
+	// subcommands.Register(subcommands.FlagsCommand(), "")
 	subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&genCmd{}, "")
 	subcommands.Register(&initCmd{}, "")
@@ -27,9 +24,9 @@ func main() {
 	allCmds := map[string]bool{
 		"commands": true, // builtin
 		"help":     true, // builtin
-		"flags":    true, // builtin
-		"gen":      true,
-		"init":     true,
+		// "flags":    true, // builtin
+		"gen":  true,
+		"init": true,
 	}
 
 	log.Printf("v%s", Version)
@@ -40,48 +37,6 @@ func main() {
 		os.Exit(int(defCmd.Execute(context.Background(), flag.CommandLine)))
 	}
 	os.Exit(int(subcommands.Execute(context.Background())))
-}
-
-type genCmd struct {
-	debug bool
-}
-
-func (*genCmd) Name() string { return "gen" }
-
-func (*genCmd) Synopsis() string {
-	return "generate handler functions and Open API specs from Go code"
-}
-
-func (*genCmd) Usage() string {
-	return `
-
-gen [packages]:
-
-generate documentation and bindings for template in specified package, defaults to "."
-	example: "zipline ./..."
-	example: "zipline gen ./..."
-	example: "zipline gen -d ./..."
-
-`
-}
-
-func (p *genCmd) SetFlags(f *flag.FlagSet) {
-	f.BoolVar(&p.debug, "d", false, "run with trace logging enabled")
-}
-
-func (p *genCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	zipline := internal.NewZipline()
-
-	debug.Debug = p.debug
-
-	err := zipline.Start(packageParam(f))
-
-	if err != nil {
-		log.Println(fmt.Errorf("%s", err.Error()))
-	}
-
-	log.Println("-----")
-	return subcommands.ExitSuccess
 }
 
 func packageParam(f *flag.FlagSet) []string {
